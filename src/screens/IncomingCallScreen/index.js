6cdd3889007143c95141728b7d5f6b40c5445dfc
Saksham pandey <saksham5701@gmail.com>
2023-10-30 01:11:10 +0530

    Completed the Incoming Call screen

--- src/screens/IncomingCallScreen/index.js
+++ src/screens/IncomingCallScreen/index.js
@@ -1,24 +1,67 @@
-import { View, Text,StyleSheet,ImageBackground } from 'react-native';
+import { View, Text,StyleSheet,ImageBackground, Pressable } from 'react-native';
 import React from 'react';
 import bg from '../../../assets/images/ios_bg.png';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Entypo from 'react-native-vector-icons/Entypo'
+import Feather from 'react-native-vector-icons/Feather'
 
 const IncomingCallScreen = () => {
+const onDecline=()=>{
+    console.warn('on Decline');
+} ;
+
+const onAccept=()=>{
+    console.warn('on Accept');
+} ;
+
+
+
+
+
   return (
     <View style={styles.root}>
-      <ImageBackground source={bg} style={styles.bg} resizeMode='cover'  >
+      <ImageBackground source={bg} style={styles.bg} resizeMode='cover'>
       <Text style={styles.name}>Shiva</Text>
       <Text style={styles.phoneNumber}> ringing +91 12456789579</Text>
-      </ImageBackground>
-      
-    </View>
+
+
+      <View style={[styles.row,{marginTop:'auto'}]}>
+       <View   style={styles.iconContainer}>
+        <Ionicons name="alarm" color="white"  size={40}/>
+            <Text style={styles.iconText}>Remind me</Text>
+        </View>
+        <View style={styles.iconContainer}>
+        <Entypo name="message" color="white"  size={40}/>
+            <Text style={styles.iconText}>Message</Text>
+        </View>
+        </View>
+
+        <View style={styles.row}>
+        {/* Decline Button */}
+        <Pressable onPress={onDecline} style={styles.iconContainer}>
+        <View style={styles.iconButtonContainer}>
+        <Feather name="x" color="white"  size={40}/>
+        </View>
+            <Text style={styles.iconText} >Decline</Text>
+        </Pressable>
+        {/* Accept Button */}
+        <Pressable onPress={onAccept} style={styles.iconContainer}>
+        <View style={[styles.iconButtonContainer,{backgroundColor:'#2e7bff'}]}>
+        <Feather name="check" color="white"  size={40} />
+        </View>
+            <Text style={styles.iconText}>Accept</Text>
+        </Pressable>
+        </View>
+        </ImageBackground>
+        </View>
+    
   );
 };
 
 const styles=StyleSheet.create({
 root:{
     height:'100%',
-    backgroundColor:'red',
-},
+   },
 name:{
     fontSize:30,
     fontWeight:'bold',
@@ -32,10 +75,31 @@ phoneNumber:{
 },
 bg:{
     backgroundColor:'red',
+    flex:1,
+    alignItems:'center',
+    padding:10,
+    paddingBottom:50,
+},
+row:{
     width:'100%',
-    height:'100%',
+    flexDirection:'row',
+    justifyContent:'space-around',
+},
+iconContainer:{
+    alignItems:'center',
+    marginVertical:20,
+},
+iconText:{
+    color:'white',
+    marginTop:10,
+},
+iconButtonContainer:{
+    backgroundColor:'red',
+    padding:10,
+    borderRadius:50,
+    magin:10,
 }
 
-})
+});
 
 export default IncomingCallScreen

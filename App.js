a0ee9037657bf3cfa608b4792f5cc207ab208f91
Saksham pandey <saksham5701@gmail.com>
2023-10-30 13:49:44 +0530

    added the call screen to the project

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { SafeAreaView,StatusBar} from 'react-native';
 import ContactsScreen from './src/screens/ContactsScreen';
 import CallingScreen from './src/screens/CallingScreen';
 import IncomingCallScreen from './src/screens/IncomingCallScreen';
+import CallScreen from './src/screens/CallScreen';
 const App = () => {
   return (
     <SafeAreaView >
     <StatusBar barStyle={'dark-content'}/>
     {/* <ContactsScreen/> */}
     {/* <CallingScreen/> */}
-    <IncomingCallScreen/>
+    {/* <IncomingCallScreen/> */}
+    <CallScreen/>
     </SafeAreaView>
   );
 };
--- src/screens/CallingScreen/index.js
+++ src/screens/CallingScreen/index.js
@@ -1,7 +1,6 @@
 import { View, Text,StyleSheet } from 'react-native'
 import React from 'react'
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import CallActionBox from '../../components/CallActionBox';
 const CallingScreen = () => {
   return (
     <View style={styles.page}>
@@ -11,21 +10,7 @@ const CallingScreen = () => {
       {/* <View style={{flex:1}}/> */}
       </View>
 
-
-      <View style={styles.buttonsContainer}>
-      <View style={styles.iconButton}>
-        <Ionicons name="camera-reverse" size={30} color={'white'}/>
-        </View>
-        <View style={styles.iconButton}>
-        <MaterialIcons name="camera-off" size={30} color={'white'}/>
-        </View>
-        <View style={styles.iconButton}>
-        <MaterialIcons name="microphone-off" size={30} color={'white'}/>
-        </View>
-        <View style={[styles.iconButton,{backgroundColor:'red'}]}>
-        <MaterialIcons name="phone-hangup" size={30} color={'white'}/>
-        </View>
-      </View>
+      <CallActionBox/>
     </View>
   );
 };
@@ -55,20 +40,7 @@ phoneNumber:{
     fontSize:20,
     color:'white',
 },
-buttonsContainer:{
-    backgroundColor:'#333333',
-    padding:20,
-    paddingBottom:40,
-    borderTopLeftRadius:15,
-    borderTopRightRadius:15,
-    flexDirection:'row',
-    justifyContent:'space-between',
-},
-iconButton:{
-backgroundColor:'#4a4a4a',
-padding: 15,
-borderRadius:50,
-},
+
 });
 
 export default CallingScreen
